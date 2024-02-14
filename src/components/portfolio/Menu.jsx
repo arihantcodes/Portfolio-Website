1cbc960a1bd23a3eac138ebf7d1240a3f1664492
arihant-jain0 <jainari1208@gmail.com>
2024-02-14 16:48:42 +0530

    add one project

--- src/components/portfolio/Menu.jsx
+++ src/components/portfolio/Menu.jsx
@@ -5,33 +5,40 @@ import Work4 from "../../assets/work-4.svg";
 import Work5 from "../../assets/work-5.svg";
 import Work6 from "../../assets/work-6.svg";
 const Menu = [
+  {
+    id: 1,
+    image: Work6,
+    title: "Motion - Notion Clone ",
+    category: "Clone",
+    link:"https://motion-note.vercel.app"
+    
+  },
+  {
+    id: 2,
+    image: Work4,
+    title: "WeaveSphere - Threads Clone ",
+    category: "Clone",
+    link:"https://weave-sphere.vercel.app/"
+    
+  },
+  {
+    id: 3,
+    image: Work2,
+    title: "Ek Chai Pilade",
+    category: "Fullstack",
+    link:"https://ekchaipilade.vercel.app/"
+    
+  },
     {
-      id: 1,
+      id: 4,
       image: Work1,
       title: "Jain Cloths ",
       category: "Fullstack",
       link:"https://jaincloths.vercel.app/"
       
     },
-    {
-      id: 2,
-      image: Work2,
-      title: "Ek Chai Pilade",
-      category: "Fullstack",
-      link:"https://ekchaipilade.vercel.app/"
-      
-    },
     
-    ,
-    {
-      id: 4,
-      image: Work4,
-      title: "WeaveSphere - Threads Clone ",
-      category: "Clone",
-      link:"https://weave-sphere.vercel.app/"
-      
-    },
-    ,
+  
     {
       id: 5,
       image: Work5,
@@ -41,13 +48,7 @@ const Menu = [
       
     },
     ,
-    {
-      id: 6,
-      image: Work6,
-      title: "Motion - Notion Clone ",
-      category: "Clone",
-      
-    },
+    
   ];
 
 export default Menu
