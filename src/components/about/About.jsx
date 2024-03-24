b2cfe7cfd9e6daf8f57ce6bf1c69554295910caa
arihant-jain0 <jainari1208@gmail.com>
2024-03-24 22:19:23 +0530

    Update About component with new description

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -6,30 +6,35 @@ const About = () => {
   return (
     <section className="about container section" id="about">
       <h2 className="section__title">About Me</h2>
-   
+
       <div className="about__data grid">
         <div className="about__info">
           <p className="about__description">
-            Hi, I'm Arihant, a Full Stack Developer with one year of experience.
-            I specialize in designing and implementing robust web applications,
-            proficient in a range of front-end and back-end technologies,
-            including Next Js,Node.js,Express Js,GraphQL,Docker,Apache Kafka and 
-            MongoDB. I'm dedicated to delivering high-quality, scalable
-            solutions that align seamlessly with client objectives. Actively
-            engaged in continuous professional development to stay at the
-            forefront of evolving industry standards.
+            Greetings!
+            <br />
+            I'm Arihant & I am Web & App Developer .
+            <br />I use Next.js and React Native to craft website & apps that not only
+            function smoothly but also bring a smile to users' faces. TypeScript
+            and GraphQL are my trusted companions in this quest for excellence.
+            In the backend, I employ Node.js, Express.js, and MongoDB to create
+            the magic that happens behind the scenes. And of course, what's a
+            great app without a touch of style? Tailwind CSS is my brush,
+            painting the perfect aesthetic picture. But it doesn't stop there. I
+            believe in keeping things organized and running like a well-oiled
+            machine. That's where Docker and AWS come into play, ensuring that
+            even when the crowds gather, everything runs like clockwork. To keep
+            track of it all, I rely on Zustand for state management. Oh, and did
+            I mention my secret weapon? C++. It's not just a language; it's a
+            tool that helps me tackle the toughest of challenges with finesse.
           </p>
           <a href="https://twitter.com/Arihantdotcom" className="btn">
             Connect On X
           </a>
         </div>
         <div className="">
-        <img src={Me} alt="" className="" />
+          <img src={Me} alt="" className="" />
+        </div>
       </div>
-      
-      
-      </div>
-      
     </section>
   );
 };
