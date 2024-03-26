215169847c2cb75260d4a4deac326df477d82ff1
arihant-jain0 <jainari1208@gmail.com>
2024-03-26 17:11:08 +0530

    Refactor About component to improve readability and update developer description

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -12,20 +12,15 @@ const About = () => {
           <p className="about__description">
             Greetings!
             <br />
-            I'm Arihant & I am Web & App Developer .
-            <br />I use Next.js and React Native to craft website & apps that not only
-            function smoothly but also bring a smile to users' faces. TypeScript
-            and GraphQL are my trusted companions in this quest for excellence.
-            In the backend, I employ Node.js, Express.js, and MongoDB to create
-            the magic that happens behind the scenes. And of course, what's a
-            great app without a touch of style? Tailwind CSS is my brush,
-            painting the perfect aesthetic picture. But it doesn't stop there. I
-            believe in keeping things organized and running like a well-oiled
-            machine. That's where Docker and AWS come into play, ensuring that
-            even when the crowds gather, everything runs like clockwork. To keep
-            track of it all, I rely on Zustand for state management. Oh, and did
-            I mention my secret weapon? C++. It's not just a language; it's a
-            tool that helps me tackle the toughest of challenges with finesse.
+            I'm Arihant & I'm a web and app developer with a passion for
+            creating modern and efficient digital solutions. With expertise in
+            technologies like React Native, Next.js, Tailwind CSS,TypeScript, and Docker, I strive
+            to build responsive, user-friendly interfaces that deliver
+            exceptional experiences. In my development journey, I leverage AWS
+            services for scalable and reliable infrastructure, MongoDB for
+            flexible and powerful database management, and Zustand for effective
+            state management, ensuring smooth data flow and application
+            performance.
           </p>
           <a href="https://twitter.com/Arihantdotcom" className="btn">
             Connect On X
